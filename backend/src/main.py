4914945ef71f433544fe49f3a0a92818c1dc9ad9
gordonzu <gordonzu@outlook.com>
2021-05-06 13:34:03 -0400

    Added flask to output data in json

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,15 +1,44 @@
+from flask import Flask, jsonify, request
 from .entities.entity import Session, engine, Base
-from .entities.exam import Exam
+from .entities.exam import Exam, ExamSchema
+
+app = Flask(__name__)
 
 Base.metadata.create_all(engine)
-session = Session()
-exams = session.query(Exam).all()
+
+@app_route('/exams')
+def get_exams():
+    session = Session()
+    exam_objects = session.query(Exam).all()
+
+    schema = Exam Schema(many=True)
+    exams = schema.dump(exam_objects)
+
+    session.close()
+    return jsonify(exams.data)
+
+@app_route('/exams', methods=['POST'])
+def add_exam():
+    posted_exam = ExamSchema(only=('title', 'description'))\
+            .load(request.get_json())
+
+    exam = Exam(**posted_exam.data, created_by="HTTP post request")
+
+    session = Session()
+    session.add(exam)
+    session.commit()
+
+    new_exam = ExamSchema().dump(exam).data
+
+    session.close()
+    return jsonify(new_exam), 201
+
+
 
 if len(exams) == 0:
     python_exam = Exam("SQLAlchemy Exam", "Test your knowledge of SQLAlchemy.", "script")
     session.add(python_exam)
     session.commit()
-    session.close()
 
     exams = session.query(Exam).all()
 
